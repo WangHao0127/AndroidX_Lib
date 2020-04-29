3f313b4c014ea26e861b85e0c01591a5472b58d0
wanghao <798753794@qq.com>
2020-04-29 15:34:45 +0800

    [AppBarLayout相关]

--- app/src/main/java/com/android/lib/ui/AppbarLayoutActivity.java
+++ app/src/main/java/com/android/lib/ui/AppbarLayoutActivity.java
@@ -1,9 +1,13 @@
 package com.android.lib.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.AndroidViewModel;
+import com.blankj.utilcode.util.ToastUtils;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.baselib.ui.BaseActivity;
 import com.android.lib.R;
@@ -28,7 +32,28 @@ public class AppbarLayoutActivity extends BaseActivity<ActivityAppbarLayoutBindi
 
     @Override
     protected void initViewsAndEvents() {
+        setSupportActionBar(mBinding.toolbar);
 
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(mBinding.designBottomSheet1);
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        mBinding.setClick(c -> {
+            if (behavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+                behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            } else if (behavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                ToastUtils.showShort("onStateChanged");
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+                ToastUtils.showShort("onSlide");
+            }
+        });
     }
 
     @Override
